45c97a8746d1cd384d2911c0a98fb1b380101306
Daniel <geral4x@gmail.com>
2020-01-15 22:23:25 +0000

    chore: added results and user right answer

--- Quem Quer Ser Fixe/Quem Quer Ser Fixe/Program.cs
+++ Quem Quer Ser Fixe/Quem Quer Ser Fixe/Program.cs
@@ -24,7 +24,8 @@ namespace Quem_Quer_Ser_Fixe
         struct respostas
         {
             public string respostaUser;
-            public string respostaCorrecta;
+            public string respostaCorrectaId;
+            public string respostaCorrectaNome;
             public string resposta1;
             public string resposta2;
             public string resposta3;
@@ -42,16 +43,18 @@ namespace Quem_Quer_Ser_Fixe
         {
             string NomeFic = "perguntas.txt";
             StreamWriter FicheiroEscrita = new StreamWriter(NomeFic);
-            FicheiroEscrita.WriteLine("Questao;respostaCorrecta;resposta1;resposta2;resposta3;resposta4");
-            FicheiroEscrita.WriteLine("Qual e a cor do cavalo branco de napoleao?;2;Verde;Branco;Azul;Roxo");
-            FicheiroEscrita.WriteLine("Quem e o aluno mais fixe do ISLA?;3;Bruno;Hugo;Daniel;Joao");
-            FicheiroEscrita.WriteLine("Quem e o professor mais fixe do ISLA?;1;Jose Moreira;Manuel Teixeira;Cristina;Ana");
+            FicheiroEscrita.WriteLine("Questao;respostaCorrectaId;respostaCorretaNome;resposta1;resposta2;resposta3;resposta4");
+            FicheiroEscrita.WriteLine("Qual e a cor do cavalo branco de napoleao?;2;Branco;Verde;Branco;Azul;Roxo");
+            FicheiroEscrita.WriteLine("Quem e o aluno mais fixe do ISLA?;3;Daniel;Bruno;Hugo;Daniel;Joao");
+            FicheiroEscrita.WriteLine("Quem e o professor mais fixe do ISLA?;1;Jose Moreira;Jose Moreira;Manuel Teixeira;Cristina;Ana");
+            FicheiroEscrita.WriteLine("Quem descobriu o Brasil em 1500?;4;Pedro Alvares Cabral;Pedro Cabral Alvares;Miguel Torga;Antonio Alvares Cabral;Pedro Alvares Cabral");
 
             for (int i = 0; i < vetor.Length; i++)
             {
                 FicheiroEscrita.Write(
                     vetor[i].questao + ";" +
-                    vetor[i].resposta.respostaCorrecta + ";" +
+                    vetor[i].resposta.respostaCorrectaId + ";" +
+                    vetor[i].resposta.respostaCorrectaNome + ";" +
                     vetor[i].resposta.resposta1 + ";" +
                     vetor[i].resposta.resposta2 + ";" +
                     vetor[i].resposta.resposta3 + ";" +
@@ -75,16 +78,18 @@ namespace Quem_Quer_Ser_Fixe
 
                 Array.Resize(ref vetor, i + 1);
                 vetor[i].questao = palavras[0];
-                vetor[i].resposta.respostaCorrecta = palavras[1];
-                vetor[i].resposta.resposta1 = palavras[2];
-                vetor[i].resposta.resposta2 = palavras[3];
-                vetor[i].resposta.resposta3 = palavras[4];
-                vetor[i].resposta.resposta4 = palavras[5];
+                vetor[i].resposta.respostaCorrectaId = palavras[1];
+                vetor[i].resposta.respostaCorrectaNome = palavras[2];
+                vetor[i].resposta.resposta1 = palavras[3];
+                vetor[i].resposta.resposta2 = palavras[4];
+                vetor[i].resposta.resposta3 = palavras[5];
+                vetor[i].resposta.resposta4 = palavras[6];
                 i++;
             }
             FicheiroLeitura.Close();
             return vetor;
         }//Fim lerFicheiro
+
 
         static void MainMenu()
         {
@@ -100,7 +105,14 @@ namespace Quem_Quer_Ser_Fixe
             {
                 case 1:
                     Console.Clear();
-                    Quiz();
+                    questoes[] obj = Quiz();
+
+                    
+                    int pontos = mostrarResultados(obj);
+                    Console.WriteLine("PONTOS: " + pontos);
+
+                    Pausa("continuar");
+
                     break;
                 case 2:
                     // Gerir Perguntas
@@ -119,7 +131,62 @@ namespace Quem_Quer_Ser_Fixe
             }
 
         }
-        static void Quiz()
+
+        private static int mostrarResultados(questoes[] obj)
+        {
+            int pontos = 0;
+            Header("     R E S U L T A D O S      ");
+            for (int i = 0; i < obj.Length; i++)
+            {
+                string respostaUser = obterResposta(obj[i]);
+                if (obj[i].resposta.respostaUser == obj[i].resposta.respostaCorrectaId)
+                {
+                    Console.WriteLine(" _____________________________");
+                    Console.WriteLine("|     |                       | ");
+                    Console.WriteLine("|   / | QUESTAO             > | " + obj[i].questao);
+                    Console.WriteLine("|  /  | RESPOSTA CORRECTA   > | " + obj[i].resposta.respostaCorrectaNome);
+                    Console.WriteLine("| V   | RESPOSTA UTILIZADOR > | " + respostaUser);
+                    Console.WriteLine("|_____|_______________________| ");
+                    pontos = pontos + 1;
+                }
+                else
+                {
+                    Console.WriteLine(" _____________________________");
+                    Console.WriteLine("|     |                       | ");
+                    Console.WriteLine("| \\ / | QUESTAO             > | " + obj[i].questao);
+                    Console.WriteLine("|  X  | RESPOSTA CORRECTA   > | " + obj[i].resposta.respostaCorrectaNome);
+                    Console.WriteLine("| / \\ | RESPOSTA UTILIZADOR > | " + respostaUser);
+                    Console.WriteLine("|_____|_______________________| ");
+                }
+            }
+            return pontos * obj.Length;
+        }
+
+        private static string obterResposta(questoes questoes)
+        {
+            string resposta;
+            switch (questoes.resposta.respostaUser)
+            {
+                case "1":
+                    resposta = questoes.resposta.resposta1;
+                    break;
+                case "2":
+                    resposta = questoes.resposta.resposta2;
+                    break;
+                case "3":
+                    resposta = questoes.resposta.resposta3;
+                    break;
+                case "4":
+                    resposta = questoes.resposta.resposta4;
+                    break;
+                default:
+                    resposta = "Resposta Invalida";
+                    break;
+            }
+            return resposta;
+        }
+
+        static questoes[] Quiz()
         {
             questoes[] allQuestoes = new questoes[0];
             Seed(allQuestoes);
@@ -130,9 +197,9 @@ namespace Quem_Quer_Ser_Fixe
                 FazerPergunta(allQuestoes[i]);
                 string respostaUser = Console.ReadLine();
                 allQuestoes[i].resposta.respostaUser = respostaUser;
-                if(allQuestoes[i].resposta.respostaUser == allQuestoes[i].resposta.respostaCorrecta)
+                if(allQuestoes[i].resposta.respostaUser == allQuestoes[i].resposta.respostaCorrectaId)
                 {
-                    Console.WriteLine("A resposta esta correcta!");
+                    Console.WriteLine("A resposta " + allQuestoes[i].resposta.respostaUser + " esta correcta!");
                 }
                 else
                 {
@@ -142,9 +209,8 @@ namespace Quem_Quer_Ser_Fixe
                 Console.Clear();
             }
 
-
-
             Pausa("terminar");
+                return allQuestoes;
         }
 
         static void FazerPergunta(questoes questao)
