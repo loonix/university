24f113bd234929343d1258dc50672a9966e55922
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2020-07-11 18:16:58 +0100

    chore: changed login to 1st form to load

--- Console Apps and Windows Forms/Projecto Algoritmia II/Codr/Codr/Core/Program.cs
+++ Console Apps and Windows Forms/Projecto Algoritmia II/Codr/Codr/Core/Program.cs
@@ -15,7 +15,7 @@ namespace Codr
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Login());
         }
     }
 }
