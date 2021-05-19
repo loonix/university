4c4c2e05672b6db404841d35bf55f51f913fd403
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2021-05-19 22:47:25 +0100

    chore: created clients controller

---
+++ ASP.NET/WebApplication_Vazio_Para_MVC/Controllers/ClientsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Vazio_Para_MVC.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
