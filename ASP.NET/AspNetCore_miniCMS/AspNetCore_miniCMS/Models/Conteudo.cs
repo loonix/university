201db2551e2e3ba9a4354a9284367ec92322adfd
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2021-06-20 16:42:09 +0100

    chore: added mini cms

--- ASP.NET/AspNetCore_miniCMS/AspNetCore_miniCMS/Models/Conteudo.cs
+++ ASP.NET/AspNetCore_miniCMS/AspNetCore_miniCMS/Models/Conteudo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace _05_ASP.NET_core_Layouts_CSS_e_Bootstrap.Models
 {
@@ -16,6 +17,8 @@ namespace _05_ASP.NET_core_Layouts_CSS_e_Bootstrap.Models
         [StringLength(50)]
         public string Titulo { get; set; }
 
+        [AllowHtml]
+        [UIHint("Conteudo")]
         public string Texto { get; set; }
 
         [StringLength(50)]
